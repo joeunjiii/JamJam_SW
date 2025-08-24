1926486557114f3d33e7b40740a50d0bba667571
yugwangmyeong <ykm5922@naver.com>
2025-08-24 19:16:15 +0900

    PostWriteScreen

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -6,7 +6,10 @@ module.exports = function (api) {
         ["module:react-native-dotenv", {
           "moduleName": "@env",
           "path": ".env",
-        }]
+          
+        }
+      ],
+      "react-native-reanimated/plugin",
       ]
     };
   };
